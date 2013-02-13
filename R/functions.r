a38fe33388ea8ce20f162031090592ebca0fa12b
Julien Barnier <julien@nozav.org>
2013-02-14 00:19:08 +0100

    Add functions get.so and cat.so

--- R/functions.r
+++ R/functions.r
@@ -37,6 +37,30 @@ read.tc <- function(text, ...) {
 ##' @param text text string
 ##' @param ... arguments passed to read.table
 
-readcsv.tc <- function(text) {
+readcsv.tc <- function(text, ...) {
   read.csv(textConnection(text), header=TRUE, ...)
+}
+
+##' Get code block from a SO question
+##' 
+##' @param id question id 
+##' @param index index of the code block to return (one number only)
+##' @param cat.output cat the content of the code blocks
+get.so <- function (id, index=NULL, cat.output=FALSE) { 
+  doc <- htmlParse(paste("http://stackoverflow.com/questions/",id,sep=""))
+  pre.index <- ifelse(is.null(index), "", paste("[",as.character(index),"]",sep=""))
+  xpath <- paste("//div[@id=\"question\"]//div[@class=\"post-text\"]//pre",pre.index,"/code",sep="")
+  code <- getNodeSet(doc, xpath)
+  code <- lapply(code, getChildrenStrings)
+  if (cat.output) return(invisible(lapply(code, cat)))
+  if (length(code)==1) return(unlist(code))
+  code
+}
+
+##' Cat code block content from a SO question
+##' 
+##' @param id question id 
+##' @param index index of the code block to cat (one number only)
+cat.so <- function (id, index=NULL) { 
+  get.so.code(id=id, index=index, cat.output=TRUE)
 }
