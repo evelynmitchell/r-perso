1a07a7e2594c4b0833d88202380c82edad6cff3b
Julien Barnier <julien@nozav.org>
2013-02-13 17:22:21 +0100

    Add functions read from textConnection()

--- R/functions.r
+++ R/functions.r
@@ -20,3 +20,23 @@ dev2png <- function(path="/tmp/temp.png") {
   dev <- dev.copy(png, file=path, width=480, height=480)
   dev.off(dev)
 }
+
+##' Read table data directly from a string
+##' 
+##' @title 
+##' @param text text string
+##' @param ... arguments passed to read.table
+
+read.tc <- function(text, ...) {
+  read.table(textConnection(text), header=TRUE, ...)
+}
+
+##' Read CSV data directly from a string
+##' 
+##' @title 
+##' @param text text string
+##' @param ... arguments passed to read.table
+
+readcsv.tc <- function(text) {
+  read.csv(textConnection(text), header=TRUE, ...)
+}
